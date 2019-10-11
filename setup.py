1cc5cd7f5f01a0cc5a95833f550e75fc081bbcd7
Jack Greisman <Jack.Greisman@gmail.com>
2019-10-11 09:54:09 -0400

    Add setup.py to enable installation to conda environment

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='reciprocalspaceship',
+    version='0.1.0',
+    author='Kevin M. Dalton; Jack B. Greisman',
+    author_email='kmdalton@g.harvard.edu; greisman@g.harvard.edu',
+    packages=find_packages(),
+    description='Tools for exploring reciprocal space',
+    install_requires=[
+        "gemmi >= 0.3.1"
+    ],
+)
