03fc21be6970047ed93ebada5c0b15e10120fa62
Jack Greisman <Jack.Greisman@gmail.com>
2019-10-17 17:43:35 -0400

    Initialize formats/mtz.py with read()

---
+++ reciprocalspaceship/formats/mtz.py
@@ -0,0 +1,12 @@
+import gemmi
+
+def read(self, mtzfile):
+    """
+    Populate the crystal object with data from an MTZ reflection file.
+
+    Parameters
+    ----------
+    mtzfile : str or file
+        name of an mtz file or a file object
+    """
+    return self
