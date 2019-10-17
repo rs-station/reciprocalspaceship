24524a18c2c2fe4659f404870d8bc242e45a4079
Jack Greisman <Jack.Greisman@gmail.com>
2019-10-17 17:55:37 -0400

    Complete mtz.read() to populate Crystal object from MTZ file

--- reciprocalspaceship/formats/mtz.py
+++ reciprocalspaceship/formats/mtz.py
@@ -1,3 +1,5 @@
+import numpy as np
+import pandas as pd
 import gemmi
 
 def read(self, mtzfile):
@@ -9,4 +11,19 @@ def read(self, mtzfile):
     mtzfile : str or file
         name of an mtz file or a file object
     """
+    mtzgemmi = gemmi.read_mtz_file(mtzfile)
+
+    # Copy data
+    all_data = np.array(mtzgemmi, copy=False)
+    F = pd.DataFrame(data=all_data, columns=mtzgemmi.column_labels())
+
+    for k,v in F.items():
+        self[k] = v
+    self.set_index(["H", "K", "L"], inplace=True)
+
+    
+    # Set Crystal attributes
+    self.cell = mtzgemmi.cell
+    self.spacegroup = mtzgemmi.spacegroup
+
     return self
