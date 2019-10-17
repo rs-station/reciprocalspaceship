101e91655a804a91700752a488f44188c76efcba
Jack Greisman <Jack.Greisman@gmail.com>
2019-10-17 18:12:50 -0400

    Update version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='reciprocalspaceship',
-    version='0.1.0',
+    version='0.1.1',
     author='Kevin M. Dalton; Jack B. Greisman',
     author_email='kmdalton@g.harvard.edu; greisman@g.harvard.edu',
     packages=find_packages(),
