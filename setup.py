88cf6a33af3e72a5a76b6d3c5c412bf6c6ef6f44
Jack Greisman <Jack.Greisman@gmail.com>
2019-10-23 22:00:47 -0400

    Bump minor version to 0.2.0:     - Added custom dtypes to enable persistence of MTZ datatypes     - Add mtz.write() function to write out Crystal objects as MTZs     - Update read() methods to assign correct MTZ-based datatypes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='reciprocalspaceship',
-    version='0.1.2',
+    version='0.2.0',
     author='Kevin M. Dalton; Jack B. Greisman',
     author_email='kmdalton@g.harvard.edu; greisman@g.harvard.edu',
     packages=find_packages(),
