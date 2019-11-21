d52e1b31a21a6eaa1afc6432e5f3a6e711873caa
Jack Greisman <Jack.Greisman@gmail.com>
2019-11-21 16:47:19 -0500

    Fix SyntaxError in Crystal.apply_symop()

--- reciprocalspaceship/crystal.py
+++ reciprocalspaceship/crystal.py
@@ -134,7 +134,7 @@ class Crystal(pd.DataFrame):
         # TODO: This for loop can be removed if gemmi.Op.apply_to_hkl() were vectorized
         for i, h in enumerate(hkl):
             newhkl[i] = symop.apply_to_hkl(h)
-            phase_shifts = symop.phase_shifts(*h)
+            phase_shifts = symop.phase_shift(*h)
             
         if inplace:
             self.reset_index(inplace=True)
