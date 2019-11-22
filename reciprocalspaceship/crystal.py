Bug fix -- correct syntax error in Crystal.apply_symop() that produced incorrect results

reciprocalspaceship/crystal.py
@@ -135,7 +135,7 @@ class Crystal(pd.DataFrame):
         # TODO: This for loop can be removed if gemmi.Op.apply_to_hkl() were vectorized
         for i, h in enumerate(hkl):
             newhkl[i] = symop.apply_to_hkl(h)
-            phase_shifts = symop.phase_shift(*h)
+            phase_shifts[i] = symop.phase_shift(*h)
         phase_shifts = np.rad2deg(phase_shifts)
             
         if inplace: