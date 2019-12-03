a0c2f9198b1fa2959ed7321b7d10e48a7381328f
Kevin Dalton <kmdalton@pop-os.localdomain>
2019-12-03 11:06:29 -0500

    fixed but in relative path

--- tests/test_hkl_to_asu.py
+++ tests/test_hkl_to_asu.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 from os import listdir
-from os.path import dirname
+from os.path import dirname,abspath
 import re
 import reciprocalspaceship as rs
 
@@ -13,23 +13,13 @@ except:
 
 class TestSymmetryOps(unittest.TestCase):
     def test_hkl_to_asu(self):
-        datadir = dirname(__file__) + '/data/'
+        datadir = abspath(dirname(__file__)) + '/data/'
         files = [datadir + i for i in listdir(datadir) if re.match(r'.*(?<!_p1).mtz$', i)] 
         for inFN in tqdm(files):
             x = rs.read_mtz(inFN)
             y = rs.read_mtz(inFN[:-4] + '_p1.mtz')
             y.spacegroup = x.spacegroup
-            yasu = y.hkl_to_asu() #if not x.spacegroup.is_reference_setting(): #    from IPython import embed
-            #    embed()
-            #print(f"{inFN}")
-            #print(f"{x.spacegroup}")
-            #print(f"{x.spacegroup.basisop}")
-            #print(f"Reference setting = {x.spacegroup.is_reference_setting()}")
-            #print(f"\t|merged - mapped| = {len(x.index.difference(yasu.index))}")
-            #print(f"\t|mapped - merged| = {len(yasu.index.difference(x.index))}")
-            #print(f"\t|merged| = {len(x)}")
-            #print(f"\t|mapped| = {len(yasu)}")
-            #There should be the same indices in merged and p1 mapped to asu
+            yasu = y.hkl_to_asu() 
             self.assertEqual(len(x.index.difference(yasu.index)), 0)
             self.assertEqual(len(yasu.index.difference(x.index)), 0)
 
