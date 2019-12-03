bc0d10a5e28011e02aeb915d925f1ecf941587c0
Jack Greisman <Jack.Greisman@gmail.com>
2019-12-03 11:45:25 -0500

    Add compute_internal_differences() method
    - Computes internal difference maps based on given symmetry operation

--- reciprocalspaceship/utils/__init__.py
+++ reciprocalspaceship/utils/__init__.py
@@ -1,5 +1,7 @@
 from .plots import plot_reciprocal_space_coverage
 from .phases import canonicalize_phases
-from .structurefactors import to_structurefactor, from_structurefactor
+from .structurefactors import (to_structurefactor,
+                               from_structurefactor,
+                               compute_internal_differences)
 from .weights import compute_doeke_weights
 from .localscale import localscale
