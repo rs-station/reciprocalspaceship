c0a0e05c7f201f119267ddac8602ea24f28219f2
Kevin Dalton <kmdalton@pop-os.localdomain>
2020-01-06 11:10:46 -0500

    do not import matplotlib by default  Changes to be committed: 	modified:   __init__.py                 This no longer imports utils/plots.py so matplotlib is                 not a dependency by default.

--- reciprocalspaceship/utils/__init__.py
+++ reciprocalspaceship/utils/__init__.py
@@ -1,4 +1,4 @@
-from .plots import plot_reciprocal_space_coverage
+#from .plots import plot_reciprocal_space_coverage
 from .phases import canonicalize_phases
 from .structurefactors import (to_structurefactor,
                                from_structurefactor,
