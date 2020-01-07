9e9ab1e56e8565afe7213a24d098c0ebfcc3fe03
Jack Greisman <Jack.Greisman@gmail.com>
2020-01-07 14:34:30 -0500

    Refactor HendricksonLattmanDtype to subclass NumpyFloat32ExtensionDtype

--- reciprocalspaceship/dtypes/phase.py
+++ reciprocalspaceship/dtypes/phase.py
@@ -1,4 +1,3 @@
-import numpy as np
 from pandas.core.dtypes.dtypes import register_extension_dtype
 
 from .base import NumpyExtensionArray, NumpyFloat32ExtensionDtype
@@ -22,29 +21,13 @@ PhaseArray._add_arithmetic_ops()
 PhaseArray._add_comparison_ops()
 
 @register_extension_dtype
-class HendricksonLattmanDtype(ExtensionDtype):
+class HendricksonLattmanDtype(NumpyFloat32ExtensionDtype):
     """
     Dtype for representing phase probability coefficients 
-    (Hendrickson-Lattman)
+    (Hendrickson-Lattman) in reflection tables
     """
-    
     name = 'HendricksonLattman'
-    type = np.float32
-    kind = 'f'
-    na_value = np.nan
     mtztype = "A"
-    
-    @property
-    def _is_numeric(self):
-        return True
-    
-    @classmethod
-    def construct_from_string(cls, string):
-        if string == cls.name:
-            return cls()
-        else:
-            raise TypeError("Cannot construct a '{}' from "
-                            "'{}'".format(cls, string))
 
     @classmethod
     def construct_array_type(cls):
