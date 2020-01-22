0cf2d67d6ac730b1d633792d67df7c9606b41adf
Jack Greisman <Jack.Greisman@gmail.com>
2020-01-22 17:16:36 -0500

    Bump version to 0.4.0 due to refactor of MTZDtypes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='reciprocalspaceship',
-    version='0.3.0',
+    version='0.4.0',
     author='Kevin M. Dalton; Jack B. Greisman',
     author_email='kmdalton@g.harvard.edu; greisman@g.harvard.edu',
     packages=find_packages(),
