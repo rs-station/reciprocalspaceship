52a02459de6d8b6d329c733cd0823edcfb0b0343
Jack Greisman <Jack.Greisman@gmail.com>
2020-04-02 12:03:07 -0400

    Remove utility functions specifically for EFX analysis

--- reciprocalspaceship/utils/__init__.py
+++ reciprocalspaceship/utils/__init__.py
@@ -1,10 +1,7 @@
-#from .plots import plot_reciprocal_space_coverage
 from .phases import canonicalize_phases
 from .structurefactors import (to_structurefactor,
                                from_structurefactor,
                                compute_internal_differences,
                                compute_structurefactor_multiplicity)
-from .weights import compute_doeke_weights
-from .localscale import localscale
 from .symop import apply_to_hkl, phase_shift
 from .rfree import add_rfree, copy_rfree
