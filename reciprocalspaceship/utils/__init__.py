490c823feca05b5dd5b932c42e0bd0d4d1bf9e65
Jack Greisman <Jack.Greisman@gmail.com>
2020-04-13 16:24:39 -0400

    Remove utils.compute_internal_differences() from __init__

--- reciprocalspaceship/utils/__init__.py
+++ reciprocalspaceship/utils/__init__.py
@@ -1,7 +1,6 @@
 from .phases import canonicalize_phases
 from .structurefactors import (to_structurefactor,
                                from_structurefactor,
-                               compute_internal_differences,
                                compute_structurefactor_multiplicity)
 from .symop import apply_to_hkl, phase_shift
 from .rfree import add_rfree, copy_rfree
