8069f63cc4459a77d4ff9bb2eaf147202bb2409b
Jack Greisman <Jack.Greisman@gmail.com>
2020-04-15 15:46:33 -0400

    Clean up rs.io namespace to use precognition instead of hkl

--- reciprocalspaceship/io/__init__.py
+++ reciprocalspaceship/io/__init__.py
@@ -1,3 +1,3 @@
-from .io import read_hkl, read_mtz
+from .io import read_precognition, read_mtz
 from .formats.mtz import write as write_mtz
-from .formats.hkl import write as write_hkl
+from .formats.precognition import write as write_precognition
