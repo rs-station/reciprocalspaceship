6aa72e6b8b995e736bf61410e8181acb2e9776e0
Jack Greisman <Jack.Greisman@gmail.com>
2020-05-05 09:18:56 -0400

    Refactor tests involving spacegroups to use pytest.fixture

--- tests/utils/test_hkl_to_asu.py
+++ tests/utils/test_hkl_to_asu.py
@@ -1,40 +1,29 @@
-import unittest
+import pytest
 import numpy as np
-from os import listdir
-from os.path import dirname, abspath, basename
-import re
 import reciprocalspaceship as rs
 
-try:
-    from tqdm import tqdm
-except:
-    tqdm = iter
+def test_hkl_to_asu(mtz_by_spacegroup):
+    # Test DataSet.hkl_to_asu() for each spacegroup
+    
+    x = rs.read_mtz(mtz_by_spacegroup)
+    y = rs.read_mtz(mtz_by_spacegroup[:-4] + '_p1.mtz')
+    y.spacegroup = x.spacegroup
 
+    # These spacegroups do not seem to be parsed using expected
+    # hexagonal/rhombohedral convention
+    if x.spacegroup.number in {146, 155}:
+        pytest.xfail(reason="gemmi handling of R3-related spacegroups")
+        
+    yasu = y.hkl_to_asu() 
+    assert len(x.index.difference(yasu.index)) == 0
+    assert len(yasu.index.difference(x.index)) == 0
 
-class TestSymmetryOps(unittest.TestCase):
-    def test_hkl_to_asu(self):
-        datadir = abspath(dirname(__file__)) + '/../data/fmodel/'
-        files = [datadir + i for i in listdir(datadir) if re.match(r'.*(?<!_p1).mtz$', i)] 
-        for inFN in tqdm(files):
-            x = rs.read_mtz(inFN)
-            y = rs.read_mtz(inFN[:-4] + '_p1.mtz')
-            y.spacegroup = x.spacegroup
+    Fx    = x.loc[yasu.index, 'FMODEL'].values.astype(float) 
+    Fyasu = yasu['FMODEL'].values.astype(float) 
+    assert np.isclose(Fx, Fyasu).min()
 
-            with self.subTest(sg=x.spacegroup.number, mtz=basename(inFN)):
-
-                yasu = y.hkl_to_asu() 
-                self.assertEqual(len(x.index.difference(yasu.index)), 0)
-                self.assertEqual(len(yasu.index.difference(x.index)), 0)
-
-                Fx    = x.loc[yasu.index, 'FMODEL'].values.astype(float) 
-                Fyasu = yasu['FMODEL'].values.astype(float) 
-                self.assertTrue(np.isclose(Fx, Fyasu).min())
-
-                Phx    = x.loc[yasu.index, 'PHIFMODEL'].values.astype(float) 
-                Phyasu = yasu['PHIFMODEL'].values.astype(float) 
-                Sx    = Fx*np.exp(1j*np.deg2rad(Phx))
-                Syasu = Fyasu*np.exp(1j*np.deg2rad(Phyasu))
-                self.assertTrue(np.isclose(Sx, Syasu, rtol=1e-3).min())
-
-if __name__ == '__main__':
-    unittest.main()
+    Phx    = x.loc[yasu.index, 'PHIFMODEL'].values.astype(float) 
+    Phyasu = yasu['PHIFMODEL'].values.astype(float) 
+    Sx    = Fx*np.exp(1j*np.deg2rad(Phx))
+    Syasu = Fyasu*np.exp(1j*np.deg2rad(Phyasu))
+    assert np.isclose(Sx, Syasu, rtol=1e-3).min()
