7b5587905e27edcfcd69cb7abd98a362d6e8314b
Jack Greisman <Jack.Greisman@gmail.com>
2020-05-12 15:46:26 -0400

    Refactor test_multiplicity.py to use pytest for confirming epsilon values

--- tests/utils/conftest.py
+++ tests/utils/conftest.py
@@ -1,7 +1,37 @@
 import pytest
+from os.path import dirname, abspath, join
+import pandas as pd
 import gemmi
 
 @pytest.fixture(params=[gemmi.SpaceGroup(n) for n in [1, 4, 5, 19, 152]])
 def common_spacegroup(request):
     """Yields common space groups for macromolecular crystals"""
+    return request.param
+
+def epsilon_factors():
+    """
+    Load epsilon factors for each extended Hermann-Mauguin (xhm) symbol.
+
+    Returns
+    -------
+    reference_data : DataFrameGroupBy
+        GroupBy iterator for epsilon reference data, grouped by xhm 
+        symbol
+    """
+    datadir = abspath(join(dirname(__file__), "../data/epsilon_factors"))
+    inFN = join(datadir, "epsilon_factors.txt.bz2")
+    reference_data = pd.read_csv(inFN)
+    return reference_data.groupby("xhm")
+
+@pytest.fixture(params=epsilon_factors())
+def epsilon_by_xhm(request):
+    """
+    Epsilon factors grouped by extended Hermann-Manguin symbol for 
+    testing.
+
+    Yields
+    ------
+    Tuple(xhm_str, DataFrame)
+        xhm symbol and reference data DataFrame
+    """
     return request.param
--- tests/utils/test_multiplicity.py
+++ tests/utils/test_multiplicity.py
@@ -1,47 +1,24 @@
-import unittest
+import pytest
 import numpy as np
-from os.path import dirname, abspath
-import re
 import reciprocalspaceship as rs
 import pandas as pd
 import gemmi
 
-try:
-    from tqdm import tqdm
-except:
-    tqdm = iter
+def test_multiplicity_epsilon(epsilon_by_xhm):
+    """
+    Test rs.utils.compute_structurefactor_multiplicity using reference 
+    data generated from sgtbx.
+    """
+    xhm = epsilon_by_xhm[0]
+    reference = epsilon_by_xhm[1]
 
-#These are repeated twice in the list output from sgtbx.
-#One of them will fail do to incorrect setting. 
-#Unsure how to fix this issue, but I think it is not
-#going to effect any realistic use cases.
-exclude_hall_symbols = {
-    ' A 2 2 -1ab', 
-    ' B 2 2 -1ab', 
-    ' C 2 2 -1ac', 
-    #These are temporary to remove R3 spacegroup from tests
-    ' R 3 -2"c',
-    ' R 3 2"',
-    '-R 3 2"',
-    ' R 3 -2"',
-    '-R 3',
-    '-R 3 2"c',
-    ' R 3',
-}
+    # Hexagonal lattices seem to cause issues:
+    if ":H" in xhm:
+        pytest.xfail("Issues with hexagonal lattices")
+    
+    H = reference[['h', 'k', 'l']].to_numpy()
+    reference_epsilon = reference['epsilon'].to_numpy()
+    groupops = gemmi.SpaceGroup(xhm).operations()
+    epsilon = rs.utils.compute_structurefactor_multiplicity(H, groupops)
+    assert np.array_equal(epsilon, reference_epsilon)
 
-class TestMultiplicityCalculation(unittest.TestCase):
-    def test_epsilon(self):
-        inFN = abspath(dirname(__file__)) + '/../data/epsilon_factors/epsilon_factors.txt.bz2'
-        reference_data = pd.read_csv(inFN)
-        keys = set(reference_data.hall.unique()) - exclude_hall_symbols
-        for key in tqdm(keys):
-            with self.subTest(spacegroup=key):
-                data = reference_data[reference_data.hall == key]
-                H = data[['h', 'k', 'l']].to_numpy()
-                reference_epsilon = data['epsilon'].to_numpy()
-                sg = gemmi.symops_from_hall(key)
-                epsilon = rs.utils.compute_structurefactor_multiplicity(H, sg)
-                self.assertTrue(np.all(epsilon == reference_epsilon))
-
-if __name__ == '__main__':
-    unittest.main()
