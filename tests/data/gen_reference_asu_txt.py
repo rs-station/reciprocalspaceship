d3e14637701647b7b0069d35f803777ef0775d0e
Kevin Dalton <kmdalton@fas.harvard.edu>
2020-05-18 14:56:19 -0400

    consolidated reference asu data

--- tests/data/gen_reference_asu_txt.py
+++ tests/data/gen_reference_asu_txt.py
@@ -18,37 +18,15 @@ dname = path.dirname(abspath) + "/asu"
 if not path.exists(dname):                
     mkdir(dname)                          
 
-outFN = dname + '/inside.csv'
-
 
 hmin,hmax = -5, 5
 H = np.mgrid[hmin:hmax+1:1,hmin:hmax+1:1,hmin:hmax+1:1].reshape((3, -1)).T
 
 
-with open(outFN, 'w') as f:
-    f.write("xhm,hall,hm,h,k,l,in_asu\n")
-    for s in tqdm(list(sgtbx.space_group_symbol_iterator())):
-        hall = s.hall()
-        hm   = s.hermann_mauguin()
-        xhm  = s.universal_hermann_mauguin()
-        sg   = sgtbx.space_group(s)
-        asu = sgtbx.reciprocal_space_asu(sg.type())
-        in_asu = [asu.is_inside(i) for i in H]
-        for h,value in zip(H, in_asu):
-            f.write(','.join([
-                xhm,
-                hall,
-                hm,
-                str(h[0]),
-                str(h[1]),
-                str(h[2]),
-                str(value),
-            ]) + '\n')
-
-outFN = dname + '/remapped.csv'
+outFN = dname + '/asu.csv'
 
 with open(outFN, 'w') as f:
-    f.write("xhm,hall,hm,h,k,l,h_asu,k_asu,l_asu\n")
+    f.write("xhm,hall,hm,h,k,l,h_asu,k_asu,l_asu,in_asu\n")
     for s in tqdm(list(sgtbx.space_group_symbol_iterator())):
         hall = s.hall()
         hm   = s.hermann_mauguin()
@@ -56,7 +34,8 @@ with open(outFN, 'w') as f:
         sg   = sgtbx.space_group(s)
         asu = sgtbx.reciprocal_space_asu(sg.type())
         H_asu = np.vstack([miller.asym_index(sg, asu, i).h() for i in H])
-        for h,h_asu in zip(H, H_asu):
+        in_asu = [asu.is_inside(i) for i in H]
+        for h,h_asu,inside in zip(H, H_asu, in_asu):
             f.write(','.join([
                 xhm,
                 hall,
@@ -67,12 +46,15 @@ with open(outFN, 'w') as f:
                 str(h_asu[0]),
                 str(h_asu[1]),
                 str(h_asu[2]),
+                str(inside),
             ]) + '\n')
 
 EOF
 
 python - << EOF
 
+import gemmi 
+import numpy as np
 import pandas as pd
 from os import remove,path
 
@@ -80,18 +62,36 @@ from os import remove,path
 abspath = path.abspath(__file__)          
 dname = path.dirname(abspath) + "/asu" 
 
-inFN = dname + '/inside.csv'
+inFN = dname + '/asu.csv'
 
 df = pd.read_csv(inFN)
-df.to_csv(inFN + '.bz2', index=False)
 
-remove(inFN)
+df['isym'] = 0
 
-inFN = dname + '/remapped.csv'
+def isym(df):
+    xhm = df['xhm'].iloc[0]
+    sg = gemmi.SpaceGroup(xhm)
+    m = gemmi.Mtz()
+    m.spacegroup = sg
+    m.add_dataset('crystal')
+    m.add_column('H', 'H')
+    m.add_column('K', 'H')
+    m.add_column('L', 'H')
+    m.add_column('M/ISYM', 'Y')
+    m.set_data(df[['h', 'k', 'l', 'isym']].to_numpy(dtype=np.float32))
+    success = m.switch_to_asu_hkl()
 
-df = pd.read_csv(inFN)
-df.to_csv(inFN + '.bz2', index=False)
+    if not success:
+        raise ValueError("Mtz.switch_to_asu_hkl retval was False! Failed to map to ASU")
 
+    df['h_gemmi'] = m.column_with_label('H').array.astype(int)
+    df['k_gemmi'] = m.column_with_label('K').array.astype(int)
+    df['l_gemmi'] = m.column_with_label('L').array.astype(int)
+    df['isym']    = m.column_with_label('M/ISYM').array.astype(int)
+    return df
+
+result = df.groupby('xhm', as_index=False).apply(isym)
+result.to_csv(inFN + '.bz2', index=False)
 remove(inFN)
 
 EOF
