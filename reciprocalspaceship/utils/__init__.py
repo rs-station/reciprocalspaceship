ac08442fb5c889c9fd08008a7cb7d3108121d38c
Jack Greisman <Jack.Greisman@gmail.com>
2020-05-28 10:28:20 -0400

    Fix spacing in utils/__init__.py

--- reciprocalspaceship/utils/__init__.py
+++ reciprocalspaceship/utils/__init__.py
@@ -5,4 +5,4 @@ from .structurefactors import (to_structurefactor,
                                is_centric)
 from .symop import apply_to_hkl, phase_shift
 from .rfree import add_rfree, copy_rfree
-from .asu import hkl_is_absent,hkl_to_asu,in_asu
+from .asu import hkl_is_absent, hkl_to_asu, in_asu
