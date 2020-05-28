239e9cf62c537e5b69dfa3a1ff40d3cdf5066736
Jack Greisman <Jack.Greisman@gmail.com>
2020-05-28 09:15:12 -0400

    Update conftest.py to make data_hewl fixture available for all tests

--- tests/algorithms/conftest.py
+++ tests/algorithms/conftest.py
@@ -4,16 +4,6 @@ import pandas as pd
 import numpy as np
 import reciprocalspaceship as rs
 
-@pytest.fixture
-def data_hewl():
-    """
-    Load HEWL diffraction data from APS 24-ID-C
-    """
-    datapath = ["..", "data", "algorithms", "HEWL_SSAD_24IDC.mtz"]
-    inFN = abspath(join(dirname(__file__), *datapath))
-    mtz = rs.read_mtz(inFN)
-    return mtz
-
 @pytest.fixture(params=["hewl",
                         "hewl_IMEAN_NaN",
                         "hewl_I(+)_NaN"])
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,31 @@
 import pytest
 from os import listdir
 from os.path import dirname, abspath, join
+import numpy as np
+import pandas as pd
 import re
+import reciprocalspaceship as rs
+
+@pytest.fixture
+def dataset_hkl():
+    """
+    Build DataSet for testing containing only Miller indices
+    """
+    hmin, hmax = -5, 5
+    H = np.mgrid[hmin:hmax+1:2,hmin:hmax+1:2,hmin:hmax+1:2].reshape((3, -1)).T
+    dataset = rs.DataSet({"H": H[:, 0], "K": H[:, 1], "L": H[:, 2]})
+    dataset.set_index(["H", "K", "L"], inplace=True)
+    return dataset
+    
+@pytest.fixture
+def data_hewl():
+    """
+    Load HEWL diffraction data from APS 24-ID-C
+    """
+    datapath = ["data", "algorithms", "HEWL_SSAD_24IDC.mtz"]
+    inFN = abspath(join(dirname(__file__), *datapath))
+    mtz = rs.read_mtz(inFN)
+    return mtz
 
 def get_mtz_by_spacegroup():
     """
