73d28b2d99d0ba3fb8fc6ee65f3d7d5b41b264d8
Jack Greisman <Jack.Greisman@gmail.com>
2020-06-26 15:36:28 -0400

    Add test for rs.print_mtz_dtypes()

---
+++ tests/test_print_mtz_dtypes.py
@@ -0,0 +1,17 @@
+import pytest
+import reciprocalspaceship as rs
+
+def test_print_mtz_dtypes(capsys):
+    """Test rs.print_mtz_dtypes()"""
+    rs.print_mtz_dtypes()
+    out, err = capsys.readouterr()
+    out = out.splitlines()
+
+    # stderr should be empty
+    assert err == ""
+
+    # check stdout
+    assert len(out[0].split()) == 4
+    assert len(out) == 18
+    for line in out[1:]:
+        assert len(line.split()) == 3
