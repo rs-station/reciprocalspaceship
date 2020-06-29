280ea1cff26b62945b13ad4ee3f6e74e8dd0fb8f
Jack Greisman <Jack.Greisman@gmail.com>
2020-06-29 13:47:48 -0400

    Add test for inplace flag of DataSet.hkl_to_asu()

--- tests/test_dataset_hkl_to_asu.py
+++ tests/test_dataset_hkl_to_asu.py
@@ -2,23 +2,28 @@ import pytest
 import numpy as np
 import reciprocalspaceship as rs
 
-def test_hkl_to_asu(mtz_by_spacegroup):
-    # Test DataSet.hkl_to_asu() for each spacegroup
-    
+@pytest.mark.parametrize("inplace", [True, False])
+def test_hkl_to_asu(mtz_by_spacegroup, inplace):
+    """Test DataSet.hkl_to_asu() for common spacegroups"""
     x = rs.read_mtz(mtz_by_spacegroup)
     y = rs.read_mtz(mtz_by_spacegroup[:-4] + '_p1.mtz')
     y.spacegroup = x.spacegroup
 
-    yasu = y.hkl_to_asu() 
+    yasu = y.hkl_to_asu(inplace=inplace)
     assert len(x.index.difference(yasu.index)) == 0
     assert len(yasu.index.difference(x.index)) == 0
 
     Fx    = x.loc[yasu.index, 'FMODEL'].values.astype(float) 
     Fyasu = yasu['FMODEL'].values.astype(float) 
-    assert np.isclose(Fx, Fyasu).all()
+    assert np.allclose(Fx, Fyasu)
 
     Phx    = x.loc[yasu.index, 'PHIFMODEL'].values.astype(float) 
     Phyasu = yasu['PHIFMODEL'].values.astype(float) 
     Sx    = Fx*np.exp(1j*np.deg2rad(Phx))
     Syasu = Fyasu*np.exp(1j*np.deg2rad(Phyasu))
-    assert np.isclose(Sx, Syasu, rtol=1e-3).all()
+    assert np.allclose(Sx, Syasu, rtol=1e-3)
+
+    if inplace:
+        assert id(yasu) == id(y)
+    else:
+        assert id(yasu) != id(y)
