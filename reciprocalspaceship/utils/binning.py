c3d2523a2043a2871b904cbf132cc9343656ae74
Jack Greisman <Jack.Greisman@gmail.com>
2020-09-04 16:02:08 -0400

    Fix handling of "rightmost" datapoint in bin_by_percentile()

--- reciprocalspaceship/utils/binning.py
+++ reciprocalspaceship/utils/binning.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def bin_by_percentile(data, bins=20, ascending=True, format_str=".3f"):
+def bin_by_percentile(data, bins=20, ascending=True, format_str=".2f"):
     """
     Bin data by percentile.
 
@@ -23,11 +23,18 @@ def bin_by_percentile(data, bins=20, ascending=True, format_str=".3f"):
     """
     if ascending:
         order = 1
+        right = False
     else:
         order = -1
+        right = True
         
     bin_edges = np.percentile(data, np.linspace(0, 100, bins+1)[::order])
-    assignments = np.digitize(data, bins=bin_edges)
-    bin_labels = np.array([ f"{edge1:{format_str}} - {edge2:{format_str}}" for edge1, edge2 in zip(bin_edges[0:-1], bin_edges[1:]) ])
+
+    # 0-index bins and ensure that right-most datapoint is in correct bin
+    assignments = np.digitize(data, bins=bin_edges, right=right) - 1
+    rightmost = np.where(assignments == bins)[0]
+    assignments[rightmost[0]] -= 1
+
+    bin_labels = [ f"{edge1:{format_str}} - {edge2:{format_str}}" for edge1, edge2 in zip(bin_edges[0:-1], bin_edges[1:]) ]
 
     return assignments, bin_labels
