60d57209ee9e9cacc05508bd684a302e7af9cb0b
Jack Greisman <Jack.Greisman@gmail.com>
2020-11-04 18:18:15 -0500

    Fix test coverage for DataSet.expand_to_p1()

--- tests/test_dataset_hkl_to_asu.py
+++ tests/test_dataset_hkl_to_asu.py
@@ -53,3 +53,8 @@ def test_expand_to_p1(mtz_by_spacegroup):
     assert_index_equal(result.index, expected.index)
     assert np.allclose(result_sf.to_numpy(), expected_sf.to_numpy(), rtol=1e-4)
     
+
+def test_expand_to_p1_unmerged(data_unmerged):
+    """Test DataSet.expand_to_p1() raises ValueError with unmerged data"""
+    with pytest.raises(ValueError):
+        result = data_unmerged.expand_to_p1()
