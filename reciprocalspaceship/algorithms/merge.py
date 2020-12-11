4f548cabdf0de0883c3e1bf567f565774a366b32
Jack Greisman <Jack.Greisman@gmail.com>
2020-12-11 16:29:44 -0500

    Add tests for rs.algorithms.merge()

--- reciprocalspaceship/algorithms/merge.py
+++ reciprocalspaceship/algorithms/merge.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def merge(dataset, intensity_key="I", sigma_key="SIGI"):
+def merge(dataset, intensity_key="I", sigma_key="SIGI", sort=False):
     """
     Merge dataset using inverse-variance weights.
 
@@ -12,6 +12,8 @@ def merge(dataset, intensity_key="I", sigma_key="SIGI"):
         Column name for intensities
     sigma_key : str
         Column name for uncertainties
+    sort : bool
+        If True, index of returned DataSet will be sorted
 
     Returns
     -------
@@ -24,8 +26,8 @@ def merge(dataset, intensity_key="I", sigma_key="SIGI"):
 
     # Map observations to reciprocal ASU
     ds = dataset.hkl_to_asu(anomalous=True)
-    ds["w"]  = ds['SIGI']**-2
-    ds["wI"] = ds["I"] * ds["w"]
+    ds["w"]  = ds[sigma_key]**-2
+    ds["wI"] = ds[intensity_key] * ds["w"]
     g = ds.groupby(["H", "K", "L"])
     
     result = g[["w", "wI"]].sum()
@@ -37,13 +39,18 @@ def merge(dataset, intensity_key="I", sigma_key="SIGI"):
     # Reshape anomalous data and use to compute IMEAN / SIGIMEAN
     result = result.unstack_anomalous()
     result.loc[:, ["N(+)", "N(-)"]] = result[["N(+)", "N(-)"]].fillna(0).astype("I")
-    result["IMEAN"] = result[["wI(+)", "wI(-)"]].sum(axis=1) / result[["w(+)", "w(-)"]].sum(axis=1)
+    result["IMEAN"] = result[["wI(+)", "wI(-)"]].sum(axis=1) / result[["w(+)", "w(-)"]].sum(axis=1).astype("Intensity")
     result["SIGIMEAN"] =  np.sqrt(1 / (result[["w(+)", "w(-)"]].sum(axis=1))).astype("Stddev")
     
     # Adjust SIGIMEAN for centric reflections due to duplicated values in
     # Friedel columns
     centrics = result.label_centrics()["CENTRIC"]
     result.loc[centrics, "SIGIMEAN"] *= np.sqrt(2)
+
+    result = result[["IMEAN", "SIGIMEAN", "I(+)", "SIGI(+)", "I(-)", "SIGI(-)", "N(+)", "N(-)"]]
+
+    if sort:
+        return result.sort_index()
     
-    return result[["IMEAN", "SIGIMEAN", "I(+)", "SIGI(+)", "I(-)", "SIGI(-)", "N(+)", "N(-)"]]
+    return result
 
---
+++ tests/algorithms/test_merge.py
@@ -0,0 +1,41 @@
+import pytest
+import numpy as np
+import reciprocalspaceship as rs
+
+
+def test_merge_valueerror(hewl_merged):
+    """
+    Confirm rs.algorithms.merge() raises ValueError when invoked with 
+    merged DataSet
+    """
+    with pytest.raises(ValueError):
+        merged = rs.algorithms.merge(hewl_merged)
+
+
+@pytest.mark.parametrize("keys", [
+    None,
+    ["I", "SIGI"],
+    ["I", "SigI"],
+])
+@pytest.mark.parametrize("sort", [True, False])
+def test_merge(hewl_unmerged, hewl_merged, keys, sort):
+    """Test rs.algorithms.merge() against AIMLESS output"""
+    if keys is None:
+        merged = rs.algorithms.merge(hewl_unmerged, sort=sort)
+    elif not (keys[0] in hewl_unmerged.columns and keys[1] in hewl_unmerged.columns):
+        with pytest.raises(KeyError):
+            merged = rs.algorithms.merge(hewl_unmerged, keys[0], keys[1], sort=sort)
+        return
+    else:
+        merged = rs.algorithms.merge(hewl_unmerged, keys[0], keys[1], sort=sort)
+
+    # Check DataSet attributes
+    assert merged.merged
+    assert merged.spacegroup.xhm() == hewl_merged.spacegroup.xhm()
+    assert merged.cell.a == hewl_merged.cell.a
+    assert merged.index.is_lexsorted() == sort
+    
+    # Note: AIMLESS zero-fills empty observations, whereas we use NaNs
+    for key in merged.columns:
+        assert np.allclose(merged.loc[hewl_merged.index, key].fillna(0),
+                           hewl_merged.loc[hewl_merged.index, key])
