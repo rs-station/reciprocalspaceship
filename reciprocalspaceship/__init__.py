68520bf0fb317767f3fdc95d2ce64ea0ba0bc684
Egor Marin <marin@phystech.edu>
2021-02-10 23:44:00 +0300

    Add read_crystfel to both __init__.py

--- reciprocalspaceship/__init__.py
+++ reciprocalspaceship/__init__.py
@@ -1,14 +1,17 @@
 # Version number for reciprocalspaceship
 def getVersionNumber():
     import pkg_resources
+
     version = pkg_resources.require("reciprocalspaceship")[0].version
     return version
+
+
 __version__ = getVersionNumber()
 
 # Top-Level API
 from .dataset import DataSet
 from .dataseries import DataSeries
-from .io import read_mtz, read_precognition, read_csv
+from .io import read_mtz, read_precognition, read_csv, read_crystfel
 from .dtypes import summarize_mtz_dtypes
 from .concat import concat
 
@@ -18,21 +21,21 @@ from reciprocalspaceship import algorithms
 # Add support for MTZ data types:
 # see http://www.ccp4.ac.uk/html/f2mtz.html
 from .dtypes import (
-    HKLIndexDtype,                           # H
-    IntensityDtype,                          # J
-    StructureFactorAmplitudeDtype,           # F
-    AnomalousDifferenceDtype,                # D
-    StandardDeviationDtype,                  # Q
-    FriedelStructureFactorAmplitudeDtype,    # G
-    StandardDeviationFriedelSFDtype,         # L
-    FriedelIntensityDtype,                   # K
-    StandardDeviationFriedelIDtype,          # M
-    NormalizedStructureFactorAmplitudeDtype, # E
-    PhaseDtype,                              # P
-    WeightDtype,                             # W
-    HendricksonLattmanDtype,                 # A
-    BatchDtype,                              # B
-    M_IsymDtype,                             # Y
-    MTZIntDtype,                             # I
-    MTZRealDtype                             # R
+    HKLIndexDtype,  # H
+    IntensityDtype,  # J
+    StructureFactorAmplitudeDtype,  # F
+    AnomalousDifferenceDtype,  # D
+    StandardDeviationDtype,  # Q
+    FriedelStructureFactorAmplitudeDtype,  # G
+    StandardDeviationFriedelSFDtype,  # L
+    FriedelIntensityDtype,  # K
+    StandardDeviationFriedelIDtype,  # M
+    NormalizedStructureFactorAmplitudeDtype,  # E
+    PhaseDtype,  # P
+    WeightDtype,  # W
+    HendricksonLattmanDtype,  # A
+    BatchDtype,  # B
+    M_IsymDtype,  # Y
+    MTZIntDtype,  # I
+    MTZRealDtype,  # R
 )
--- reciprocalspaceship/io/__init__.py
+++ reciprocalspaceship/io/__init__.py
@@ -7,5 +7,6 @@ from .mtz import (
 from .precognition import (
     read_precognition,
 )
+from .crystfel import (read_crystfel)
 from .ccp4map import write_ccp4_map
 from .csv import read_csv
