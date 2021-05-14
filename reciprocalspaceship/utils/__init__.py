599aa3728a97f3262fcde7a1fefcdece861559a8
Kevin Dalton <kevinmdalton@gmail.com>
2021-05-14 13:10:48 -0400

    fix order to avoid circular import

--- reciprocalspaceship/utils/__init__.py
+++ reciprocalspaceship/utils/__init__.py
@@ -6,7 +6,7 @@ from .structurefactors import (to_structurefactor,
                                is_absent)
 from .symop import apply_to_hkl, phase_shift
 from .rfree import add_rfree, copy_rfree
-from .asu import hkl_to_asu, hkl_to_observed, in_asu, generate_reciprocal_asu
 from .cell import compute_dHKL, generate_reciprocal_cell
+from .asu import hkl_to_asu, hkl_to_observed, in_asu, generate_reciprocal_asu
 from .binning import bin_by_percentile
 from .units import ev2angstroms, angstroms2ev
