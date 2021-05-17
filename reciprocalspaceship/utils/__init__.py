7e88bf7f8257b1048c562a5792eb1f5307b5d4a4
Kevin Dalton <kevinmdalton@gmail.com>
2021-05-17 12:16:49 -0400

    fix imports after moving redundancy.py to utils

--- reciprocalspaceship/utils/__init__.py
+++ reciprocalspaceship/utils/__init__.py
@@ -10,3 +10,4 @@ from .cell import compute_dHKL, generate_reciprocal_cell
 from .asu import hkl_to_asu, hkl_to_observed, in_asu, generate_reciprocal_asu
 from .binning import bin_by_percentile
 from .units import ev2angstroms, angstroms2ev
+from .stats import compute_redundancy
--- tests/utils/test_stats.py
+++ tests/utils/test_stats.py
@@ -1,7 +1,6 @@
 import pytest
 import numpy as np
 import reciprocalspaceship as rs
-from reciprocalspaceship import stats
 import gemmi
 
 @pytest.mark.parametrize("cell_and_spacegroup", [
@@ -11,14 +10,14 @@ import gemmi
 @pytest.mark.parametrize("anomalous", [False, True])
 def test_compute_redundancy(cell_and_spacegroup, anomalous):
     """ 
-    Test reciprocalspaceship.stats.compute_redundancy.
+    Test reciprocalspaceship.utils.compute_redundancy.
     """
     dmin = 5.
     cell,spacegroup = cell_and_spacegroup
     hkl = rs.utils.generate_reciprocal_asu(cell, spacegroup, dmin, anomalous=anomalous)
     mult = np.random.choice(10, size=len(hkl))
     hobs = np.repeat(hkl, mult, axis=0)
-    result = stats.compute_redundancy(hobs, cell, spacegroup, anomalous)
+    result = rs.utils.compute_redundancy(hobs, cell, spacegroup, anomalous)
     assert len(hkl) == len(mult)
     assert np.all(result.loc[(i for i in hkl)].iloc[:,0] == mult.astype(float))
 
