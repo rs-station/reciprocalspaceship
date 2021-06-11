4298c4c099489c01df36cc27521195184430a4f1
Jack Greisman <Jack.Greisman@gmail.com>
2021-06-11 15:05:36 -0400

    Add __all__ to rs.utils to help sphinx docs

--- reciprocalspaceship/utils/__init__.py
+++ reciprocalspaceship/utils/__init__.py
@@ -1,3 +1,28 @@
+# Public API for `reciprocalspaceship.utils`
+__all__ = [
+    "canonicalize_phases",
+    "get_phase_restrictions",
+    "to_structurefactor",
+    "from_structurefactor",
+    "compute_structurefactor_multiplicity",
+    "is_centric",
+    "is_absent",
+    "apply_to_hkl",
+    "phase_shift",
+    "add_rfree",
+    "copy_rfree",
+    "compute_dHKL",
+    "generate_reciprocal_cell",
+    "hkl_to_asu",
+    "hkl_to_observed",
+    "in_asu",
+    "generate_reciprocal_asu",
+    "bin_by_percentile",
+    "ev2angstroms",
+    "angstroms2ev",
+    "compute_redundancy",
+]
+
 from .phases import canonicalize_phases, get_phase_restrictions
 from .structurefactors import (to_structurefactor,
                                from_structurefactor,
