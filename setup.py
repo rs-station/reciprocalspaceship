a1eca176fcefd2f2079f639ed0225b7721e22527
Jack Greisman <Jack.Greisman@gmail.com>
2021-07-06 17:23:12 -0400

    Add testing requirements to [dev] install

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup, find_packages
 
 # Get version number
 def getVersionNumber():
-    with open("reciprocalspaceship/VERSION", 'r') as vfile:
+    with open("reciprocalspaceship/VERSION", "r") as vfile:
         version = vfile.read().strip()
     return version
+
+
 __version__ = getVersionNumber()
 
 DESCRIPTION = "Tools for exploring reciprocal space"
@@ -34,18 +36,40 @@ PROJECT_URLS = {
     "Source Code": "https://github.com/Hekstra-Lab/reciprocalspaceship",
 }
 
+# Testing requirements
+tests_require = ["pytest", "pytest-cov", "pytest-xdist"]
+
+# Documentation requirements
+docs_require = [
+    # sphinx documentation
+    "sphinx",
+    "sphinx_rtd_theme",
+    "nbsphinx",
+    "sphinx-panels",
+    "sphinxcontrib-autoprogram",
+    "jupyter",
+    "autodocsumm",
+    # example notebooks
+    "tqdm",
+    "matplotlib",
+    "seaborn",
+    "celluloid",
+    "scikit-image",
+    "torch",
+]
+
 setup(
-    name='reciprocalspaceship',
+    name="reciprocalspaceship",
     packages=find_packages(),
     version=__version__,
-    license='MIT',
+    license="MIT",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
-    author='Kevin M. Dalton, Jack B. Greisman',
-    author_email='kmdalton@g.harvard.edu, greisman@g.harvard.edu',
+    author="Kevin M. Dalton, Jack B. Greisman",
+    author_email="kmdalton@g.harvard.edu, greisman@g.harvard.edu",
     url="https://hekstra-lab.github.io/reciprocalspaceship/",
     project_urls=PROJECT_URLS,
-    python_requires='>3.6',
+    python_requires=">3.6",
     install_requires=[
         "gemmi >= 0.4.2",
         "pandas >= 1.2.0, < 1.3",
@@ -53,41 +77,22 @@ setup(
         "scipy",
         "ipython",
     ],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'pytest-cov', 'pytest-xdist'],
-    extras_require={
-        'dev': [
-            # sphinx documentation
-            "sphinx",
-            "sphinx_rtd_theme",
-            "nbsphinx",
-            "sphinx-panels",
-            "sphinxcontrib-autoprogram",
-            "jupyter",
-            "autodocsumm",
-            
-            # example notebooks
-            "tqdm",
-            "matplotlib",
-            "seaborn",
-            "celluloid",
-            "scikit-image",
-            "torch",
-        ]
-    },
+    setup_requires=["pytest-runner"],
+    tests_require=tests_require,
+    extras_require={"dev": tests_require + docs_require},
     entry_points={
-        'console_scripts': [
-            'rs.mtzdump=reciprocalspaceship.commandline.mtzdump:main',
+        "console_scripts": [
+            "rs.mtzdump=reciprocalspaceship.commandline.mtzdump:main",
         ]
     },
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Scientific/Engineering :: Chemistry',
-        'Topic :: Scientific/Engineering :: Physics',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Programming Language :: Python',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
     ],
 )
