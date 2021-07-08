882ca73368aee15635f0ffe41ee7c53226caecf1
Jack Greisman <Jack.Greisman@gmail.com>
2021-07-08 11:31:58 -0400

    Fix numpy deprecation warnings in tests

--- tests/algorithms/test_scale_merged_intensities.py
+++ tests/algorithms/test_scale_merged_intensities.py
@@ -199,17 +199,17 @@ def test_fw_posterior_quad(ref_method, data_fw_cctbx, data_fw_mcmc):
     """
     if ref_method == 'cctbx':
         I,SigI,Sigma,J,SigJ,F,SigF,Centric = data_fw_cctbx.to_numpy(np.float64).T
-        Centric = Centric.astype(np.bool)
+        Centric = Centric.astype(bool)
         rtol = 0.06
         rs_J,rs_SigJ,rs_F,rs_SigF = _french_wilson_posterior_quad(I, SigI, Sigma, Centric)
     elif ref_method == 'mcmc':
         I,SigI,Sigma,J,SigJ,F,SigF,Centric = data_fw_mcmc.to_numpy(np.float64).T
-        Centric = Centric.astype(np.bool)
+        Centric = Centric.astype(bool)
         rtol = 0.025
         rs_J,rs_SigJ,rs_F,rs_SigF = _french_wilson_posterior_quad(I, SigI, Sigma, Centric)
     elif ref_method == 'mcmc vs cctbx':
         I,SigI,Sigma,J,SigJ,F,SigF,Centric = data_fw_mcmc.to_numpy(np.float64).T
-        Centric = Centric.astype(np.bool)
+        Centric = Centric.astype(bool)
         _,_,_,rs_J,rs_SigJ,rs_F,rs_SigF,_ = data_fw_cctbx.to_numpy(np.float64).T
         rtol = 0.06
 
