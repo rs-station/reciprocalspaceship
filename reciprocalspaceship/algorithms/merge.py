ecd0370e12bf4b368f5f75fac5009b408d43508a
Jack Greisman <Jack.Greisman@gmail.com>
2021-08-25 16:15:44 -0400

    Run black on reciprocalspaceship/algorithms

--- reciprocalspaceship/algorithms/merge.py
+++ reciprocalspaceship/algorithms/merge.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def merge(dataset, intensity_key="I", sigma_key="SIGI", sort=False):
     """
     Merge dataset using inverse-variance weights.
@@ -22,14 +23,16 @@ def merge(dataset, intensity_key="I", sigma_key="SIGI", sort=False):
     """
 
     if dataset.merged:
-        raise ValueError("rs.algorithms.merge() can only be called with an unmerged DataSet")
+        raise ValueError(
+            "rs.algorithms.merge() can only be called with an unmerged DataSet"
+        )
 
     # Map observations to reciprocal ASU
     ds = dataset.hkl_to_asu(anomalous=True)
-    ds["w"]  = ds[sigma_key]**-2
+    ds["w"] = ds[sigma_key] ** -2
     ds["wI"] = ds[intensity_key] * ds["w"]
     g = ds.groupby(["H", "K", "L"])
-    
+
     result = g[["w", "wI"]].sum()
     result["I"] = result["wI"] / result["w"]
     result["SIGI"] = np.sqrt(1 / result["w"]).astype("Stddev")
@@ -39,18 +42,23 @@ def merge(dataset, intensity_key="I", sigma_key="SIGI", sort=False):
     # Reshape anomalous data and use to compute IMEAN / SIGIMEAN
     result = result.unstack_anomalous()
     result.loc[:, ["N(+)", "N(-)"]] = result[["N(+)", "N(-)"]].fillna(0).astype("I")
-    result["IMEAN"] = result[["wI(+)", "wI(-)"]].sum(axis=1) / result[["w(+)", "w(-)"]].sum(axis=1).astype("Intensity")
-    result["SIGIMEAN"] =  np.sqrt(1 / (result[["w(+)", "w(-)"]].sum(axis=1))).astype("Stddev")
-    
+    result["IMEAN"] = result[["wI(+)", "wI(-)"]].sum(axis=1) / result[
+        ["w(+)", "w(-)"]
+    ].sum(axis=1).astype("Intensity")
+    result["SIGIMEAN"] = np.sqrt(1 / (result[["w(+)", "w(-)"]].sum(axis=1))).astype(
+        "Stddev"
+    )
+
     # Adjust SIGIMEAN for centric reflections due to duplicated values in
     # Friedel columns
     centrics = result.label_centrics()["CENTRIC"]
     result.loc[centrics, "SIGIMEAN"] *= np.sqrt(2)
 
-    result = result[["IMEAN", "SIGIMEAN", "I(+)", "SIGI(+)", "I(-)", "SIGI(-)", "N(+)", "N(-)"]]
+    result = result[
+        ["IMEAN", "SIGIMEAN", "I(+)", "SIGI(+)", "I(-)", "SIGI(-)", "N(+)", "N(-)"]
+    ]
 
     if sort:
         return result.sort_index()
-    
-    return result
 
+    return result
