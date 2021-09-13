a97dd4b17a617bb1f273fb453018ac50dc5b52a2
Jack Greisman <Jack.Greisman@gmail.com>
2021-09-13 12:40:58 -0400

    Add cellify decorator to coerce input arguments to gemmi.UnitCell

--- reciprocalspaceship/decorators.py
+++ reciprocalspaceship/decorators.py
@@ -52,6 +52,7 @@ def range_indexed(f):
 
 def _convert_spacegroup(val):
     """Helper method to try to convert value to gemmi.SpaceGroup"""
+    # GH#18: Type-checking for supported input types
     if isinstance(val, gemmi.SpaceGroup) or (val is None):
         return val
     elif isinstance(val, (str, int)):
@@ -74,8 +75,12 @@ def spacegroupify(func=None, *sg_args):
 
     When specified with argument names, such as ``@spacegroupify("parent_sg")``, only
     the provided argument names are coerced to gemmi.SpaceGroup. The provided argument
-    needs to exactly match an argument in the decorated function's call signature in order
+    needs to exactly match an argument in the decorated function's call signature
     for this decorator to coerce the input values.
+
+    Note
+    ----
+        ``None`` values are passed to the decorated function unchanged.
     """
     if not callable(func) and func is not None:
         sg_args = (func, *sg_args)
@@ -92,6 +97,61 @@ def spacegroupify(func=None, *sg_args):
             for arg in sg_args:
                 if arg in bargs.arguments:
                     bargs.arguments[arg] = _convert_spacegroup(bargs.arguments[arg])
+            return f(**bargs.arguments)
+
+        return wrapped
+
+    return _decorator(func) if callable(func) else _decorator
+
+
+def _convert_unitcell(val):
+    """Helper method to try to convert value to gemmi.UnitCell"""
+    # GH#18: Type-checking for supported input types
+    if isinstance(val, gemmi.UnitCell) or (val is None):
+        return val
+    elif isinstance(val, (list, tuple, np.ndarray)) and len(val) == 6:
+        return gemmi.UnitCell(*val)
+    else:
+        raise ValueError(f"Cannot construct gemmi.UnitCell from value: {val}")
+
+
+def cellify(func=None, *cell_args):
+    """
+    A decorator that converts unit cell arguments to gemmi.UnitCell objects.
+
+    This decorator facilitates writing methods that use gemmi.UnitCell objects
+    by allowing the method to accept unit cell arguments as tuples, lists, numpy
+    arrays, or gemmi.UnitCell without needing to write boilerplate argument
+    checking code.
+
+    When specified as ``@cellify`` or ``@cellify()``, any arguments named
+    "cell", "unit_cell", or "unitcell" are automatically coerced to provide
+    gemmi.UnitCell values.
+
+    When specified with argument names, such as ``@cellify("uc")``, only
+    the provided argument names are coerced to gemmi.UnitCell. The provided argument
+    needs to exactly match an argument in the decorated function's call signature
+    for this decorator to coerce the input values.
+
+    Note
+    ----
+        ``None`` values are passed to the decorated function unchanged.
+    """
+    if not callable(func) and func is not None:
+        cell_args = (func, *cell_args)
+
+    if len(cell_args) == 0:
+        cell_args = ("cell", "unit_cell", "unitcell")
+
+    def _decorator(f):
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+            sig = signature(f)
+            bargs = sig.bind(*args, **kwargs)
+            bargs.apply_defaults()
+            for arg in cell_args:
+                if arg in bargs.arguments:
+                    bargs.arguments[arg] = _convert_unitcell(bargs.arguments[arg])
             return f(**bargs.arguments)
 
         return wrapped
