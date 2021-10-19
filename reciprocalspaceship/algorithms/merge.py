5a46858eba84dc14db778fb2245545aeb1dc7824
Kevin Dalton <kevinmdalton@gmail.com>
2021-10-19 12:26:58 -0400

    add anomalous flag to rs.algorightms.merge

--- reciprocalspaceship/algorithms/merge.py
+++ reciprocalspaceship/algorithms/merge.py
@@ -1,19 +1,21 @@
 import numpy as np
 
 
-def merge(dataset, intensity_key="I", sigma_key="SIGI", sort=False):
+def merge(dataset, intensity_key="I", sigma_key="SIGI", anomalous=True, sort=False):
     """
     Merge dataset using inverse-variance weights.
 
     Parameters
     ----------
-    dataset : rs.DataSet
+    dataset : rs.DataSet (optional)
         Unmerged DataSet containing scaled intensities and uncertainties
     intensity_key : str
         Column name for intensities
-    sigma_key : str
+    sigma_key : str (optional)
         Column name for uncertainties
-    sort : bool
+    anomalous : bool (optional)
+        If True, do not merge Friedel mates
+    sort : bool (optional)
         If True, index of returned DataSet will be sorted
 
     Returns
@@ -28,7 +30,7 @@ def merge(dataset, intensity_key="I", sigma_key="SIGI", sort=False):
         )
 
     # Map observations to reciprocal ASU
-    ds = dataset.hkl_to_asu(anomalous=True)
+    ds = dataset.hkl_to_asu(anomalous=anomalous)
     ds["w"] = ds[sigma_key] ** -2
     ds["wI"] = ds[intensity_key] * ds["w"]
     g = ds.groupby(["H", "K", "L"])
@@ -40,23 +42,28 @@ def merge(dataset, intensity_key="I", sigma_key="SIGI", sort=False):
     result.merged = True
 
     # Reshape anomalous data and use to compute IMEAN / SIGIMEAN
-    result = result.unstack_anomalous()
-    result.loc[:, ["N(+)", "N(-)"]] = result[["N(+)", "N(-)"]].fillna(0).astype("I")
-    result["IMEAN"] = result[["wI(+)", "wI(-)"]].sum(axis=1) / result[
-        ["w(+)", "w(-)"]
-    ].sum(axis=1).astype("Intensity")
-    result["SIGIMEAN"] = np.sqrt(1 / (result[["w(+)", "w(-)"]].sum(axis=1))).astype(
-        "Stddev"
-    )
+    if anomalous:
+        result = result.unstack_anomalous()
+        result.loc[:, ["N(+)", "N(-)"]] = result[["N(+)", "N(-)"]].fillna(0).astype("I")
+        result["IMEAN"] = result[["wI(+)", "wI(-)"]].sum(axis=1) / result[
+            ["w(+)", "w(-)"]
+        ].sum(axis=1).astype("Intensity")
+        result["SIGIMEAN"] = np.sqrt(1 / (result[["w(+)", "w(-)"]].sum(axis=1))).astype(
+            "Stddev"
+        )
 
-    # Adjust SIGIMEAN for centric reflections due to duplicated values in
-    # Friedel columns
-    centrics = result.label_centrics()["CENTRIC"]
-    result.loc[centrics, "SIGIMEAN"] *= np.sqrt(2)
+        # Adjust SIGIMEAN for centric reflections due to duplicated values in
+        # Friedel columns
+        centrics = result.label_centrics()["CENTRIC"]
+        result.loc[centrics, "SIGIMEAN"] *= np.sqrt(2)
 
-    result = result[
-        ["IMEAN", "SIGIMEAN", "I(+)", "SIGI(+)", "I(-)", "SIGI(-)", "N(+)", "N(-)"]
-    ]
+        result = result[
+            ["IMEAN", "SIGIMEAN", "I(+)", "SIGI(+)", "I(-)", "SIGI(-)", "N(+)", "N(-)"]
+        ]
+    else:
+        result = result[
+            ["I", "SIGI", "N"]
+        ]
 
     if sort:
         return result.sort_index()
