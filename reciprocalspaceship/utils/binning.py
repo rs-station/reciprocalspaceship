a0813f6ad01a2abbaa691cbbbc67161917518309
Jack Greisman <Jack.Greisman@gmail.com>
2022-03-26 13:36:33 -0400

    Add convenience function for assigning bins from bin edges

--- reciprocalspaceship/utils/binning.py
+++ reciprocalspaceship/utils/binning.py
@@ -1,7 +1,18 @@
 import numpy as np
 
 
-def bin_by_percentile(data, bins=20, ascending=True, format_str=".2f"):
+def assign_with_binedges(data, bin_edges, right=True):
+    """Assign data using given bin edges"""
+    # 0-index bins and ensure that right-most datapoint is in correct bin
+    assignments = np.digitize(data, bins=bin_edges, right=right) - 1
+    rightmost = np.where(assignments == (len(bin_edges) - 1))[0]
+    assignments[rightmost] -= 1
+    return assignments
+
+
+def bin_by_percentile(
+    data, bins=20, ascending=True, return_edges=False, format_str=".2f"
+):
     """
     Bin data by percentile.
 
@@ -13,6 +24,8 @@ def bin_by_percentile(data, bins=20, ascending=True, format_str=".2f"):
         Number of bins
     ascending : bool
         Whether to bin data by value from low to high
+    return_edges: bool
+        Whether to return the bin edges
     format_str : str
         Format string for constructing bin labels
 
@@ -31,14 +44,14 @@ def bin_by_percentile(data, bins=20, ascending=True, format_str=".2f"):
 
     bin_edges = np.percentile(data, np.linspace(0, 100, bins + 1)[::order])
 
-    # 0-index bins and ensure that right-most datapoint is in correct bin
-    assignments = np.digitize(data, bins=bin_edges, right=right) - 1
-    rightmost = np.where(assignments == bins)[0]
-    assignments[rightmost] -= 1
+    assignments = assign_with_binedges(data, bin_edges, right=right)
 
     bin_labels = [
         f"{edge1:{format_str}} - {edge2:{format_str}}"
         for edge1, edge2 in zip(bin_edges[0:-1], bin_edges[1:])
     ]
 
-    return assignments, bin_labels
+    if return_edges:
+        return assignments, bin_labels, bin_edges
+    else:
+        return assignments, bin_labels
