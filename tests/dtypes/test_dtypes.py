bd5871f3c8436b0323470863302d6ed2aa8a2fbd
Jack Greisman <Jack.Greisman@gmail.com>
2022-05-02 12:59:24 -0400

    Add test for to_numpy() method of MTZIntegerArray and MTZFloatArray

--- tests/dtypes/test_dtypes.py
+++ tests/dtypes/test_dtypes.py
@@ -57,6 +57,32 @@ def test_to_numpy_nan(data_int, with_nan):
         assert result.dtype.name == "int32"
 
 
+@pytest.mark.parametrize("with_nan", [True, False])
+def test_int_to_float_withNan(data_int, with_nan):
+    """
+    Test int32-backed ExtensionArray to_numpy(np.float32) method. Should return
+    float32 regardless of whether it contains a NaN.
+    """
+    if with_nan:
+        data_int[10] = data_int._na_value
+
+    result = data_int.to_numpy(np.float32)
+    assert result.dtype.name == "float32"
+
+
+@pytest.mark.parametrize("with_nan", [True, False])
+def test_float_to_float_withNan(data_float, with_nan):
+    """
+    Test float32-backed ExtensionArray to_numpy() method. Should return
+    float32 regardless of whether it contains a NaN.
+    """
+    if with_nan:
+        data_float[10] = data_float._na_value
+
+    result = data_float.to_numpy()
+    assert result.dtype.name == "float32"
+
+
 def test_is_friedel_dtype(dtype_all):
     """Test MTZDtype.is_friedel_dtype()"""
     expected = rs.DataSeries(np.arange(0, 100), dtype=dtype_all[0]())
