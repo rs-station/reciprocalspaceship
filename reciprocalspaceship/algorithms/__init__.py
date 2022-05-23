e7a536971873f4b1c7aa20f6f1e5a4f73c4d4d2a
Dennis Brookner <debrookner@gmail.com>
2022-05-22 22:18:39 -0400

    Update __init__.py
    Add new function to __init__.py

--- reciprocalspaceship/algorithms/__init__.py
+++ reciprocalspaceship/algorithms/__init__.py
@@ -2,3 +2,4 @@ from reciprocalspaceship.algorithms.merge import merge
 from reciprocalspaceship.algorithms.scale_merged_intensities import (
     scale_merged_intensities,
 )
+from reciprocalspaceship.algorithms.intensity import compute_intensity_from_structurefactor
