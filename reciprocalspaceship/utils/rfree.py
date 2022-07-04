40f16aae6422b93a79d950af57435896978c19b2
minhuanli <minhuanli@g.harvard.edu>
2022-07-04 12:05:22 -0400

    add ccp4 convention for add_free function, also fix some related bugs

--- reciprocalspaceship/utils/rfree.py
+++ reciprocalspaceship/utils/rfree.py
@@ -3,7 +3,7 @@ import numpy as np
 from reciprocalspaceship.dtypes import MTZIntDtype
 
 
-def add_rfree(dataset, fraction=0.05, bins=20, inplace=False):
+def add_rfree(dataset, fraction=0.05, bins=20, ccp4_convention=False, inplace=False):
     """
     Add an r-free flag to the dataset object for refinement.
     R-free flags are used to identify reflections which are not used in automated refinement routines.
@@ -17,6 +17,10 @@ def add_rfree(dataset, fraction=0.05, bins=20, inplace=False):
         Fraction of reflections to be added to the r-free. (the default is 0.05)
     bins : int, optional
         Number of resolution bins to divide the free reflections over. (the default is 20)
+    ccp4_conventiion: bool, optional
+        Default False to use Phenix(CNS/XPLOR) convention: !=0 is test set and key is "R-free-flags".
+        Set to True to use ccp4 convention: ==0 is test set and the key is "FreeR_flag".
+        See https://www.ccp4.ac.uk/html/freerflag.html#description for convention details.
     inplace : bool, optional
 
     Returns
@@ -33,14 +37,21 @@ def add_rfree(dataset, fraction=0.05, bins=20, inplace=False):
     bin_edges = np.percentile(dataset["dHKL"], np.linspace(100, 0, bins + 1))
     bin_edges = np.vstack([bin_edges[:-1], bin_edges[1:]]).T
 
-    dataset["R-free-flags"] = 0
-    dataset["R-free-flags"] = dataset["R-free-flags"].astype(MTZIntDtype())
+    test_set = np.random.random(len(dataset)) <= fraction
 
-    free = np.random.random(len(dataset)) <= fraction
+    if not ccp4_convention:
+        rfree_key = "R-free-flags"
+        update = test_set
+    else:
+        rfree_key = "FreeR_flags"
+        update = ~test_set
+
+    dataset[rfree_key] = 0
+    dataset[rfree_key] = dataset[rfree_key].astype(MTZIntDtype())
 
     for i in range(bins):
         dmax, dmin = bin_edges[i]
-        dataset[free & (dataset["dHKL"] >= dmin) & (dataset["dHKL"] <= dmax)] = i
+        dataset[update & (dataset["dHKL"] >= dmin) & (dataset["dHKL"] <= dmax)][rfree_key] = i+1
 
     if not dHKL_present:
         del dataset["dHKL"]
