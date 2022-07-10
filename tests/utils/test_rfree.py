a9877419048eb71d7e2ec09ff6ba31dec1bd2936
minhuanli <minhuanli@g.harvard.edu>
2022-07-10 18:41:06 -0400

    update add_rfree test from unittest to pytest style

--- tests/utils/test_rfree.py
+++ tests/utils/test_rfree.py
@@ -2,48 +2,40 @@ import unittest
 from os.path import abspath, dirname, join
 
 import numpy as np
+import pytest
 
 import reciprocalspaceship as rs
 
+@pytest.mark.parametrize("fraction", [0.05, 0.10, 0.15])
+@pytest.mark.parametrize("ccp4_convention", [False, True])
+@pytest.mark.parametrize("inplace", [False, True])
+def test_add_rfree(data_fmodel, fraction, ccp4_convention, inplace):
+    '''
+    Test rs.utils.add_rfee
+    '''
+    data_copy = data_fmodel.copy()
+    rfree = rs.utils.add_rfree(data_fmodel, fraction=fraction, ccp4_convention=ccp4_convention, inplace=inplace)
+
+    if ccp4_convention:
+        label_name = "FreeR_flag"
+        assert label_name in rfree.columns
+        assert np.sum(rfree.loc[:, label_name] == 0) / len(rfree) < fraction+0.1
+    else:
+        label_name = "R-free-flags"
+        assert label_name in rfree.columns
+        assert np.sum(rfree.loc[:, label_name] == 1) / len(rfree) < fraction+0.1
+
+    if inplace:
+        assert id(data_fmodel) == id(rfree)
+        assert label_name in data_fmodel.columns
+        assert np.all(data_copy == rfree.loc[:, rfree.columns != label_name]) 
+    else:
+        assert id(data_fmodel) != id(rfree)
+        assert label_name not in data_fmodel.columns
+        assert np.all(data_fmodel == data_copy)
+        assert np.all(data_fmodel == rfree.loc[:, rfree.columns != label_name])
 
 class TestRfree(unittest.TestCase):
-    def test_add_rfree(self):
-
-        datadir = join(abspath(dirname(__file__)), "../data/fmodel")
-        data = rs.read_mtz(join(datadir, "9LYZ.mtz"))
-
-        # Should copy data
-        rfree = rs.utils.add_rfree(data)
-        self.assertFalse(id(data) == id(rfree))
-        self.assertFalse("R-free-flags" in data.columns)
-        self.assertTrue("R-free-flags" in rfree.columns)
-
-        # Should keep all other unchanged
-        self.assertTrue(np.all(data == rfree.loc[:, rfree.columns != "R-free-flags"]))
-        # Should have fewer ==1 as test set
-        self.assertTrue(np.sum(rfree.loc[:, "R-free-flags"] == 1) / len(rfree) < 0.15)
-
-        # Test ccp4_convention
-        rfree_ccp4 = rs.utils.add_rfree(data, ccp4_convention=True)
-        self.assertTrue("FreeR_flag" in rfree_ccp4.columns)
-
-        # Should keep all other unchanged
-        self.assertTrue(
-            np.all(data == rfree_ccp4.loc[:, rfree_ccp4.columns != "FreeR_flag"])
-        )
-        # Should have fewer ==0 as test set
-        self.assertTrue(
-            np.sum(rfree_ccp4.loc[:, "FreeR_flag"] == 0) / len(rfree_ccp4) < 0.15
-        )
-
-        # Test inplace option
-        rfree = rs.utils.add_rfree(data, inplace=True)
-        self.assertTrue(id(data) == id(rfree))
-        self.assertTrue("R-free-flags" in data.columns)
-        self.assertTrue("R-free-flags" in rfree.columns)
-
-        return
-
     def test_copy_rfree(self):
 
         datadir = join(abspath(dirname(__file__)), "../data/fmodel")
