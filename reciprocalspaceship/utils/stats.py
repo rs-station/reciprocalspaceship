d9ad7b434e42ac6398e5766ede0cfc0987bd0bf4
Kevin Dalton <kmdalton@pop-os.localdomain>
2022-10-18 11:37:41 -0400

    add `rs.utils.weighted_pearsonr` and tests

--- reciprocalspaceship/utils/stats.py
+++ reciprocalspaceship/utils/stats.py
@@ -81,3 +81,41 @@ def compute_redundancy(
 
     mult = mult.sort_index()
     return mult.get_hkls(), mult["Count"].to_numpy(np.int32)
+
+def weighted_pearsonr(x, y, w):
+    """
+    Calculate a [weighted Pearson correlation coefficient](https://en.wikipedia.org/wiki/Pearson_correlation_coefficient#Weighted_correlation_coefficient).
+
+    Note
+    ----
+    x, y, and w may be arbitrarily batched. the correlation coefficient will be computed pairwise along the last axis. 
+
+    Parameters
+    ----------
+    x : np.array(float)
+        An array of observations. 
+    y : np.array(float)
+        An array of observations the same length as x.
+    w : np.array(float)
+        An array of weights the same length as x. These needn't be normalized.
+
+    Returns
+    -------
+    r : float
+        The Pearson correlation coefficient
+    """
+    z = w.sum(-1)
+
+    mx = np.reciprocal(z) * (w * x).sum(-1)
+    my = np.reciprocal(z) * (w * y).sum(-1)
+
+    dx = x - np.expand_dims(mx, axis=-1)
+    dy = y - np.expand_dims(my, axis=-1)
+
+    cxy = (w * dx * dy).sum(-1) / z
+    cx  = (w * dx * dx).sum(-1) / z
+    cy  = (w * dy * dy).sum(-1) / z
+
+    r = cxy / np.sqrt(cx * cy)
+    return r
+
---
+++ tests/utils/test_weighted_pearsonr.py
@@ -0,0 +1,37 @@
+import pytest
+import numpy as np
+from scipy.stats import pearsonr
+import reciprocalspaceship as rs
+
+
+def test_weighted_pearsonr():
+    n = 100
+
+    x, y, sigx, sigy = np.random.random((4, n))
+    w = np.sqrt(sigx*sigx + sigy*sigy)
+
+    #Test execution
+    rs.utils.stats.weighted_pearsonr(x, y, w)
+
+    #Test against scipy pearsonr
+    w = np.ones(n)
+    r = rs.utils.stats.weighted_pearsonr(x, y, w)
+    expected_r = pearsonr(x, y)[0]
+    assert np.isclose(r, expected_r)
+
+
+def test_weighted_pearsonr_batched():
+    #Test batch execution
+    a,b,n = 2,3,100
+    x, y, sigx, sigy = np.random.random((4, a, b, n))
+    w = np.sqrt(sigx*sigx + sigy*sigy)
+    r = rs.utils.stats.weighted_pearsonr(x, y, w)
+    assert np.all(np.array(r.shape) == np.array([a, b]))
+
+    #Test against scipy pearsonr
+    w = np.ones((a, b, n))
+    r = rs.utils.stats.weighted_pearsonr(x, y, w)
+    for i in range(a):
+        for j in range(b):
+            expected_r = pearsonr(x[i,j], y[i,j])[0]
+            assert np.isclose(r[i,j], expected_r)
