9339461a0ffa000416a9a8bc1f7d88e169d4832a
Doeke Hekstra <doeke_hekstra@harvard.edu>
2023-12-21 15:37:49 -0500

    switch to minimum_sigma

--- reciprocalspaceship/algorithms/scale_merged_intensities.py
+++ reciprocalspaceship/algorithms/scale_merged_intensities.py
@@ -95,7 +95,7 @@ def _french_wilson_posterior_quad(Iobs, SigIobs, Sigma, centric, npoints=100):
     return mean, np.sqrt(variance), mean_F, np.sqrt(variance_F)
 
 
-def mean_intensity_by_miller_index(I, H, bandwidth, clip_neg_Sigma=False, eps=0.001):
+def mean_intensity_by_miller_index(I, H, bandwidth):
     """
     Use a gaussian kernel smoother to compute mean intensities as a function of miller index.
 
@@ -107,9 +107,6 @@ def mean_intensity_by_miller_index(I, H, bandwidth, clip_neg_Sigma=False, eps=0.
         Nx3 array of miller indices
     bandwidth : float(optional)
         Kernel bandwidth in miller units
-    clip_neg_Sigma : bool(optional, default False)
-        If true, clips Sigma to be at least 0.0001 before calculating Sigma
-    eps (float) : small positive number to clip Sigma to (if clip_neg_Sigma=True). Default=0.001.
 
     Returns
     -------
@@ -125,9 +122,6 @@ def mean_intensity_by_miller_index(I, H, bandwidth, clip_neg_Sigma=False, eps=0.
     for i in range(n):
         K = np.exp(-0.5 * ((H - H[i]) * (H - H[i])).sum(1) / bandwidth)
         S[i] = (I * K).sum() / K.sum()
-
-    if clip_neg_Sigma:
-        S = np.clip(S, a_min=eps, a_max=1e20)
 
     return S
 
@@ -191,8 +185,7 @@ def scale_merged_intensities(
     mean_intensity_method="isotropic",
     bins=100,
     bw=2.0,
-    clip_neg_Sigma=False,
-    eps=0.001,
+    minimum_sigma=-np.inf,
 ):
     """
     Scales merged intensities using Bayesian statistics in order to
@@ -248,11 +241,8 @@ def scale_merged_intensities(
         parameter controls the distance that each reflection impacts in
         reciprocal space. Only affects output if mean_intensity_method is
         \"anisotropic\".
-    clip_neg_Sigma : bool
-        Will set negative Sigma to 0 for the purpose of calculating Sigma.
-        Addresses rare cases where local average Iobs is negative.
-        Default: False. *Used by valdo*.
-    eps : float Floor imposed on Sigma if clipping.
+    minimum_sigma : float 
+        Minimum value imposed on Sigma (default: -np.inf, that is: no minimum).
 
 
     Returns
@@ -296,15 +286,16 @@ def scale_merged_intensities(
     if mean_intensity_method == "isotropic":
         dHKL = ds["dHKL"].to_numpy(dtype=np.float64)
         Sigma = (
-            mean_intensity_by_resolution(I / multiplicity, dHKL, bins) * multiplicity
+            mean_intensity_by_resolution(I / multiplicity, dHKL, bins)
         )
     elif mean_intensity_method == "anisotropic":
         Sigma = (
             mean_intensity_by_miller_index(
-                I / multiplicity, ds.get_hkls(), bw, clip_neg_Sigma, eps
+                I / multiplicity, ds.get_hkls(), bw
             )
-            * multiplicity
         )
+    Sigma = np.clip(Sigma, a_min=minimum_sigma, a_max=np.inf)
+    Sigma = Sigma * multiplicity
 
     # Initialize outputs
     ds[outputI] = 0.0
