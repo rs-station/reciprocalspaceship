8db20f9ffbcc7087fba763cb4f3fb19debd3d9a4
dermen <dermen@lbl.gov>
2024-08-10 19:36:48 -0700

    merged precommit

--- reciprocalspaceship/commandline/cctbx2rs.py
+++ reciprocalspaceship/commandline/cctbx2rs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import glob
-import ray
 
+import ray
 
 import reciprocalspaceship as rs
 from reciprocalspaceship.utils import cctbx_to_rs as to_rs
@@ -14,15 +14,19 @@ def main():
     assert args.ucell is not None
     assert args.symbol is not None
 
-    fnames = glob.glob(args.dirname+"/*integrated.refl")
+    fnames = glob.glob(args.dirname + "/*integrated.refl")
     print("Found %d files" % len(fnames))
 
     ray.init(num_cpus=args.nj)
 
     # get the refl data
     get_refl_data = ray.remote(to_rs.get_refl_data)
-    refl_data = ray.get( [get_refl_data.remote(fnames, args.ucell, args.symbol, rank, args.nj) \
-        for rank in range(args.nj)])
+    refl_data = ray.get(
+        [
+            get_refl_data.remote(fnames, args.ucell, args.symbol, rank, args.nj)
+            for rank in range(args.nj)
+        ]
+    )
     refl_data = [ds for ds in refl_data if ds is not None]
 
     print("Combining tables!")
@@ -30,13 +34,13 @@ def main():
     expt_ids = set(ds.BATCH)
     print(f"Found {len(ds)} refls from {len(expt_ids)} expts.")
     print("Mapping batch column.")
-    expt_id_map = {name:i for i,name in enumerate(expt_ids)}
+    expt_id_map = {name: i for i, name in enumerate(expt_ids)}
     ds.BATCH = [expt_id_map[eid] for eid in ds.BATCH]
-    
-    ds.infer_mtz_dtypes().set_index(["H","K","L"], drop=True).write_mtz(args.mtz)
+
+    ds.infer_mtz_dtypes().set_index(["H", "K", "L"], drop=True).write_mtz(args.mtz)
     print("Wrote %s." % args.mtz)
     print("Done!")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
--- reciprocalspaceship/utils/cctbx_to_rs.py
+++ reciprocalspaceship/utils/cctbx_to_rs.py
@@ -1,29 +1,40 @@
-from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import glob
 import os
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser
 from copy import deepcopy
-import numpy as np
-import msgpack
-import gemmi
 
+import gemmi
+import msgpack
+import numpy as np
 
 import reciprocalspaceship as rs
-    
+
 MSGPACK_DTYPES = {
-    'double': np.float64,
-    'float': np.float32,
-    'int': np.int32,
-    'cctbx::miller::index<>': np.int32,
-    'vec3<double>': np.float64,
-    'std::size_t': np.intp}
+    "double": np.float64,
+    "float": np.float32,
+    "int": np.int32,
+    "cctbx::miller::index<>": np.int32,
+    "vec3<double>": np.float64,
+    "std::size_t": np.intp,
+}
 
 
 def get_parser():
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument("dirname", type=str, help="diffBragg.stills_process output folder with integrated.refls")
+    parser.add_argument(
+        "dirname",
+        type=str,
+        help="diffBragg.stills_process output folder with integrated.refls",
+    )
     parser.add_argument("mtz", type=str, help="output mtz name")
-    parser.add_argument("--ucell", default=None, nargs=6, type=float, help="unit cell params (default will be average experiment crystal)")
-    parser.add_argument("--symbol",type=str, default=None)
+    parser.add_argument(
+        "--ucell",
+        default=None,
+        nargs=6,
+        type=float,
+        help="unit cell params (default will be average experiment crystal)",
+    )
+    parser.add_argument("--symbol", type=str, default=None)
     return parser
 
 
@@ -32,7 +43,7 @@ def get_msgpack_data(data, name):
     if dtype in MSGPACK_DTYPES:
         dtype = MSGPACK_DTYPES[dtype]
     else:
-        dtype=None
+        dtype = None
     vals = np.frombuffer(buff, dtype).reshape((num, -1))
     return vals.T
 
@@ -50,27 +61,38 @@ def get_refl_data(fnames, ucell, symbol, rank=0, size=1):
     sg_num = gemmi.find_spacegroup_by_name(symbol).number
     all_ds = []
 
-    for i_f,f in enumerate(fnames):
+    for i_f, f in enumerate(fnames):
         if i_f % size != rank:
             continue
-        if rank==0:
+        if rank == 0:
             print(f"Loading {i_f+1}/{len(fnames)}")
-        _,_,R = msgpack.load(open(f, 'rb'), strict_map_key=False)
-        refl_data = R['data']
-        expt_id_map = R['identifiers']
+        _, _, R = msgpack.load(open(f, "rb"), strict_map_key=False)
+        refl_data = R["data"]
+        expt_id_map = R["identifiers"]
         col_names = list(refl_data.keys())
-        h,k,l = get_msgpack_data(refl_data, 'miller_index')
-        I, = get_msgpack_data(refl_data, 'intensity.sum.value')
-        sigI, = get_msgpack_data(refl_data, 'intensity.sum.variance')
-        x,y,_ = get_msgpack_data(refl_data, 'xyzcal.px')
-        sx,sy,sz = get_msgpack_data(refl_data, 's1')
-        dpsi, = get_msgpack_data(refl_data, 'delpsical.rad')
-        local_id, = get_msgpack_data(refl_data, 'id')
+        h, k, l = get_msgpack_data(refl_data, "miller_index")
+        (I,) = get_msgpack_data(refl_data, "intensity.sum.value")
+        (sigI,) = get_msgpack_data(refl_data, "intensity.sum.variance")
+        x, y, _ = get_msgpack_data(refl_data, "xyzcal.px")
+        sx, sy, sz = get_msgpack_data(refl_data, "s1")
+        (dpsi,) = get_msgpack_data(refl_data, "delpsical.rad")
+        (local_id,) = get_msgpack_data(refl_data, "id")
         global_id = np.array([expt_id_map[li] for li in local_id])
-        ds = rs.DataSet({"H":h, "K":k, "L":l,
-                "BATCH": global_id, "DPSI": dpsi,
-                "I": I, "SigI": sigI, "X":x, "Y":y},
-                cell=ucell, spacegroup=sg_num)
+        ds = rs.DataSet(
+            {
+                "H": h,
+                "K": k,
+                "L": l,
+                "BATCH": global_id,
+                "DPSI": dpsi,
+                "I": I,
+                "SigI": sigI,
+                "X": x,
+                "Y": y,
+            },
+            cell=ucell,
+            spacegroup=sg_num,
+        )
         ds["SX"] = sx
         ds["SY"] = sy
         ds["SZ"] = sz
@@ -81,4 +103,3 @@ def get_refl_data(fnames, ucell, symbol, rank=0, size=1):
     else:
         all_ds = None
     return all_ds
-
