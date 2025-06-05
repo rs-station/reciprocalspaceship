19da75495de1a2df56aa1bc9fd2325090a731a06
Kevin Dalton <kmdalton@slac.stanford.edu>
2025-06-05 11:08:50 -0400

    add separate "doc" install

--- setup.py
+++ setup.py
@@ -84,7 +84,8 @@ setup(
     setup_requires=["pytest-runner"],
     tests_require=tests_require,
     extras_require={
-        "dev": tests_require + docs_require + examples_require,
+        "dev": tests_require,
+        "doc" : docs_require + examples_require
         "examples": examples_require,
     },
     entry_points={
